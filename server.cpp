e8cf437bb5d1cd3a6427a083a5b5e9f24eb3e794
Your Name <you@example.com>
2018-05-01 04:41:26 +0000

    multiple messages

--- server.cpp
+++ server.cpp
@@ -5,6 +5,9 @@
 #include <sys/socket.h>
 //#include <sys/types.h>
 #include <netinet/in.h>
+#include <signal.h>
+
+void dostuff(int newsockfd);
 
 void error(const char *msg)
 {
@@ -12,11 +15,27 @@ void error(const char *msg)
 	exit(1);
 }
 
+bool done = false;
+
+void int_handler(int s) {
+	printf("Caught signal %d\n",s);
+	done = true;
+}
+
 int main(int argc, char** argv) {
 	if (argc < 2) {
 		fprintf(stderr, "ERROR, no port provided\n");
 		exit(1);
 	}
+
+	struct sigaction sig_int_handler;
+	sig_int_handler.sa_handler = int_handler;
+	sigemptyset(&sig_int_handler.sa_mask);
+	sig_int_handler.sa_flags = 0;
+
+	sigaction(SIGINT, &sig_int_handler, NULL);
+
+	signal(SIGCHLD, SIG_IGN);
 
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0) {
@@ -36,11 +55,32 @@ int main(int argc, char** argv) {
 
 	struct sockaddr_in cli_addr;
 	socklen_t clilen = sizeof(cli_addr);
-	int newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-	if (newsockfd < 0) {
-		error("ERROR on accept");
+
+	while (!done) {
+		int newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+		if (newsockfd < 0) {
+			error("ERROR on accept");
+		}
+		printf("-\n");
+		int pid = fork();
+		if (pid < 0) {
+			error("ERROR on fork");
+		}
+		if (pid == 0) {
+			close(sockfd);
+			dostuff(newsockfd);
+			exit(0);
+		} else {
+			close(newsockfd);
+		}
 	}
 
+	close(sockfd);
+
+	return 0;
+}
+
+void dostuff(int newsockfd) {
 	char buffer[256];
 	bzero(buffer, 256);
 
@@ -56,8 +96,5 @@ int main(int argc, char** argv) {
 	}
 
 	close(newsockfd);
-	close(sockfd);
-
-	return 0;
 }
 
